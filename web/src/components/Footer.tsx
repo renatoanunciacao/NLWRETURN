ad921d675f019acd2f0fdedc0a0182e3e571396d
Renato Wagner Anunciação <wagnerpunk002@gmail.com>
2022-06-10 17:53:10 -0300

    Ajustes no texto do footer

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -2,13 +2,13 @@ export function Footer() {
   return (
     <footer
       className="bg-slate-800
-             text-3xl text-white text-center
+             text-white text-center
              fixed
              inset-x-0
              bottom-0
              p-4 "
     >
-      &copy; Forest Time. Design: <a href="http://html.design">HTML Design</a>.
+      &copy; Todos os direitos reservados.</a>.
     </footer>
   );
 }
