3e1ad4dbd66b648c3e6c621e9b63dbe414147871
Renato Wagner Anunciação <wagnerpunk002@gmail.com>
2022-06-10 11:28:58 -0300

    Removendo o contexto de temas.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,5 @@
 import { Widget } from "./components/Widget";
-import { ThemeContextProvider } from "./contexts/ThemeContext";
-import { Theme } from "./components/Theme";
+
 export function App() {
-  return (
-    <ThemeContextProvider>
-      <Theme />
-    </ThemeContextProvider>
-  );
+  return <Widget />;
 }
